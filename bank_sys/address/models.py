10d3d94ec4e8ce25bee066cbc463a190e9fec6df
pybalt <leobravolbb2002bej@gmail.com>
2022-09-22 19:00:37 -0300

    country and state code must be unique

--- bank_sys/address/models.py
+++ bank_sys/address/models.py
@@ -13,7 +13,7 @@ class Country(models.Model):
     """
     id = models.AutoField(primary_key=True, unique = True, verbose_name = "ID")
     country_name = models.CharField(max_length = 50, unique=True,  verbose_name = "Country name")
-    code = models.CharField(max_length=20, verbose_name = "Code") # This is intend to be used with ISO 3166
+    code = models.CharField(max_length=20, unique = True, verbose_name = "Code") # This is intend to be used with ISO 3166
 
     class Meta:
         managed = True
@@ -35,8 +35,8 @@ class State(models.Model):
     code field is intended to be used with ISO3166
     """
     id = models.AutoField(primary_key=True, unique = True, verbose_name= "ID")
-    state_name = models.CharField(max_length= 50, verbose_name = "State name")
-    code = models.CharField(max_length= 20, verbose_name = "Code")
+    state_name = models.CharField(max_length= 50, unique= True, verbose_name = "State name")
+    code = models.CharField(max_length= 20, unique= True, verbose_name = "Code")
     
     class Meta:
         managed = True
