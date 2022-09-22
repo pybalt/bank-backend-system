6eff300a89eadc231684842fa5272c118939d4a5
pybalt <leobravolbb2002bej@gmail.com>
2022-09-22 14:56:20 -0300

    refactoring dir, solving mistake 'adress'

--- bank_sys/address/apps.py
+++ bank_sys/address/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AdressConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adress'
+    name = 'address'
--- bank_sys/bank_sys/settings.py
+++ bank_sys/bank_sys/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'address',
+    'branch',
+    'card',
+    'customer',
+    'employee',
+    'loan',
+    'movement',
 ]
 
 MIDDLEWARE = [
