8c19c860bd35a4bc4f22730388d7aaa3282148ef
pybalt <leobravolbb2002bej@gmail.com>
2022-10-06 19:30:43 -0300

    refactoring the algorythm

--- bank_sys/data/management/commands/fill-data.py
+++ bank_sys/data/management/commands/fill-data.py
@@ -1,6 +1,6 @@
 # This is a custom command. You use it through manage.py this way:
 "py manage.py fill-data"
-from re import I
+from types import NoneType
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from faker import Faker
@@ -12,144 +12,192 @@ import random
 from django.db.utils import IntegrityError
 gc = geonamescache.GeonamesCache()
 countries = gc.get_countries()
-cities = gc.get_cities()  
+cities = gc.get_cities()
 fake = Faker()
 
+
+class IGenerated():
+    def choose_random(self) -> dict:
+        raise NotImplementedError #  I must refactor. Users doesn't get choosed.
+
+    def does_exists(self) -> bool:
+        raise NotImplementedError
+
+    def insert(self) -> bool:
+        print(self.insert)
+        if not self.does_exists():
+            self.instance.save()
+
+    def model_object(self) -> object:
+        raise NotImplementedError
+
+
+class ISubdivissions(IGenerated):
+    def has_subdivissions() -> bool:
+        raise NotImplementedError
+
+
+class GeneratedCountry(ISubdivissions):
+
+    def choose_random(self) -> str:
+        print(self.choose_random)
+        return random.choice(list(countries))
+
+    def does_exists(self) -> bool:
+        print(self.does_exists)
+        return Country.objects.filter(code=self.country_code).exists()
+
+    def has_subdivissions(self) -> bool:
+        print(self.has_subdivissions)
+        return bool(pycountry.subdivisions.get(country_code=self.country_code))
+
+    @staticmethod
+    def model_object(country_code) -> object:
+        return Country.objects.get(code=country_code)
+
+    def __init__(self) -> None:
+        self.country_code: str = self.choose_random()
+        self.country_name: str = countries[f"{self.country_code}"]["name"]
+        if not self.has_subdivissions():
+            return self.__init__()
+        self.instance: object = Country(code=self.country_code,
+                                        country_name=self.country_name)
+
+
+class GeneratedState(ISubdivissions):
+
+    def choose_random(self, country_code: str) -> str:
+        print(self.choose_random)
+        subdivissions = pycountry.subdivisions.get(country_code=country_code)
+        return random.choice(list(subdivissions))
+
+    def does_exists(self) -> bool:
+        print(self.does_exists)
+        return State.objects.filter(code=self.state_code).exists()
+
+    def has_subdivissions(self) -> bool:
+        print(self.has_subdivissions)
+        # This is completely random. I must refactor.
+        return NotImplementedError
+
+    @staticmethod
+    def model_object(state_code) -> object:
+        return State.objects.get(code=state_code)
+
+    def __init__(self, country_code) -> None:
+        self.__state = self.choose_random(country_code)
+        self.state_code = self.__state.code
+        self.state_name = self.__state.name
+        self.instance = State(code=self.state_code,
+                              state_name=self.state_name,
+                              country=GeneratedCountry.model_object(
+                                  country_code
+                                  )
+                              )
+
+
+class GeneratedCity(IGenerated):
+
+    def choose_random(self, countrycode: str) -> str(int):
+        print(self.choose_random)
+
+        filtered_list = [
+            i for i in
+            filter(lambda x:
+                   cities[x]["countrycode"] == countrycode,
+                   cities)
+            ]
+        if filtered_list:
+            return random.choice(filtered_list)
+        else:
+            return countries[f"{countrycode}"]["name"]
+
+    def does_exists(self) -> bool:
+        print(self.does_exists)
+        return City.objects.filter(state_id=self.instance.state,
+                                   city_name=self.instance.city_name).exists()
+
+    @staticmethod
+    def model_object(city_name, state_code) -> object:
+        return City.objects.filter(city_name=city_name,
+                                   state=GeneratedState.model_object(
+                                       state_code
+                                       )
+                                   ).first()
+
+    def __init__(self, countrycode, state_code) -> None:
+        self.city_id = self.choose_random(countrycode)
+        try:
+            self.name = cities[self.city_id]["name"]
+        except KeyError:
+            self.name = self.city_id
+        self.instance = City(city_name=self.name,
+                             state=GeneratedState.model_object(
+                                  state_code
+                                  ))
+
+
 class RandomStreet:
-    
+
     def __init__(self, *args, **kwargs):
-        self.street_name = fake.street_name()
-        self.street_number = int(fake.building_number())
-    
-class RandomCountry:
-    
-    def __init__(self, *args, **kwargs):
-        self._number_of_countries = Country.objects.count() 
-        self.country_code = random.choice(list(countries))
-        self.country_name = countries[f"{self.country_code}"]["name"]
-        self.country_states:list = list(pycountry.subdivisions.get(country_code = self.country_code))
-        print(f"{[self.country_code, self.country_name]}, number of states: {len(self.country_states)}")
-        Country(country_name = self.country_name, code = self.country_code).save()
 
-    
-class RandomState:
-    
-    def __init__(self, *args, **kwargs):
-        self.country:object = RandomCountry()
-        self._number_of_states = State.objects.count()
-        self.state_info:dict = random.choice(self.country.country_states)
-        print(self.state_info)
-        self.state_name = self.state_info.name
-        self.state_code = self.state_info.code
-        State(state_name = self.state_name, code = self.state_code).save()
-        print(self.state_name, self.state_code)
-
-def city_filter(countrycode):
-    return [ i for i in filter(lambda x: cities[x]["countrycode"] == countrycode, cities) ]
-
-class RandomCity(RandomState):
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._number_of_cities = City.objects.count()
-        self._city___id:str = random.choice([i for i in city_filter(self.country.country_code)])
-        self.city_name:str = cities[self._city___id]["name"]
-        print(self.city_name)
-        City(city_name = self.city_name).save()
+        self.name = fake.street_name()
+        self.number = int(fake.building_number())
 
 
+class GeneratedAddress(IGenerated):
 
-class RandomAddress(RandomCity):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._street = RandomStreet()
-        self.address_name = self._street.street_name 
-        self.address_number = self._street.street_number
-        
-        self._countries_pks : list = Country.objects.filter(code = self.country.country_code).values_list('pk', flat=True)
-        print("countries pk:    ", self._countries_pks)
-        self._random_pk : int = random.choice(self._countries_pks)
-        self.address_country : object = Country.objects.filter(pk = self._random_pk).first()
-        
-        self._states_pks : list = State.objects.filter(code = self.state_code).values_list('pk', flat=True)
-        print("states pks:  ", self._states_pks)
-        self._random_pk : int = random.choice(self._states_pks)
-        self.address_state : object = State.objects.filter(pk = self._random_pk).first()
-        
-        self._cities_pks : list = City.objects.filter(city_name = self.city_name).values_list('pk', flat=True)
-        print("cities pks   ", self._cities_pks)
-        self._random_pk : int = random.choice(self._cities_pks)
-        self.address_city : object = City.objects.filter(pk = self._random_pk).first()
-        
+    def choose_random(self, country_code: str) -> str:
+        print(self.choose_random)
+        subdivissions = pycountry.subdivisions.get(country_code=country_code)
+        return random.choice(list(subdivissions))
 
+    def does_exists(self) -> bool:
+        print(self.does_exists)
+        return Address.objects.filter(address_name=self.address_name,
+                                      address_number=self.address_number,
+                                      address_city=self.address_city).exists()
 
-class ModelDB:
-    "This class has a method that creates model objects. "
+    def has_subdivissions(self) -> bool:
+        print(self.has_subdivissions)
+        # This is completely random. I must refactor.
+        return NotImplementedError
+
     @staticmethod
-    def address() -> object:
-        address = RandomAddress()
-        return Address(address_name = address.address_name,
-                       address_number = address.address_number,
-                       address_city = address.address_city,
-                       address_country = address.address_country,
-                       address_state = address.address_state )
-    @staticmethod
-    def user() -> object:
-        return User(username = f"{fake.simple_profile()['username']}", 
-                    password = f"{fake.password(length = 50, special_chars = True, digits = True)}")
-    @staticmethod
-    def branch(i) -> object:
-        return Branch(branch_number = random.randint(1, 9999), 
-                        branch_name = fake.color_name(),
-                        branch_address = i)
+    def model_object(name, number, city) -> object:
+        return Address.objects.get(address_name=name,
+                                   address_number=number,
+                                   address_city=city)
+
+    def __init__(self, city, state, *args, **kwargs):
+
+        self.__street = RandomStreet()
+        self.address_name = self.__street.name
+        self.address_number = self.__street.number
+        self.address_city: object = GeneratedCity.model_object(city,
+                                                               state)
+        self.instance = Address(address_name=self.address_name,
+                                address_number=self.address_number,
+                                address_city=self.address_city)
+
 
 class Command(BaseCommand):
-    
+
     help = "This command is to fill the database with dummy data. Just for testing purposes."
 
-    
     def handle(self, *args, **kwargs):
-        
-        number_of_fields = int(input("How many fields do you want to generate?  \n  --> "))
-        
-        _integrity_errors = 0
-        new_fields = 0
-        while True:
-            if new_fields == number_of_fields:
-                break
-            if _integrity_errors > 6:
-                print("You can't add more fields.")
-                break
-            try:
-                ModelDB.address().save()
-            except IntegrityError:
-                _integrity_errors += 1
-                continue
-            except IndexError:
-                "Just a Country with no subdivissions."
-                continue
-            except TypeError:
-                "A Country with no subdivissions can also throw an TypeError: NoneType."
-                continue
-            try:
-                ModelDB.user().save()
-            except IntegrityError:
-                "It seems this user already exists."
-                
-            "Branchs"
-            desired_proportion = 8 # 1 of 8 instances must be a branch 
-            number_of_addresses = Address.objects.count()
-            number_of_branchs = int("%.0f" % (number_of_addresses/desired_proportion))
-            
-            if number_of_branchs > 0:
-                for _ in range(number_of_branchs):
-                    pk_addresses : list = Address.objects.all().values_list('id')
-                    pk = random.choice(pk_addresses)
-                    try:
-                        ModelDB.branch(Address.objects.filter(pk = pk[0]).first()).save()
-                    except IntegrityError:
-                        "It seems this address has already been asigned."
-                        continue
-            new_fields += 1
-            
-            
+
+        number_of_fields = int(
+            input("How many fields do you want to generate?  \n  --> "))
+        for _ in range(number_of_fields):
+            country = GeneratedCountry()
+            print(country.country_name, country.country_code)
+            country.insert()
+            state = GeneratedState(country.country_code)
+            print(state.state_code, state.state_name, state.instance)
+            state.insert()
+            city = GeneratedCity(country.country_code,
+                                 state.state_code)
+            city.insert()
+            address = GeneratedAddress(city.name, state.state_code)
+            address.insert()
