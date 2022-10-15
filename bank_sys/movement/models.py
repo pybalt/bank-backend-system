7ab83a64c708e493b6406dda2e34e2419d89aba1
pybalt <leobravolbb2002bej@gmail.com>
2022-10-15 19:19:54 -0300

    Movement database model

--- bank_sys/movement/models.py
+++ bank_sys/movement/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+from account.models import Account
 # Create your models here.
+
+
+class Movement(models.Model):
+
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    amount = models.IntegerField(verbose_name="Amount")
+    description = models.CharField(max_length=30)
+    date = models.DateField(auto_now_add=True)
+    recipient = models.ForeignKey(Account, on_delete=models.CASCADE,
+                               verbose_name="Recipient")
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE,
+                               verbose_name="Sender")
+    
+    def clean(self):
+        if self.recipient == self.sender:
+            raise ValidationError("This operation doesn't make sense.")
+    
+    class Meta:
+        managed = True
+        db_table = 'movement'
+        verbose_name = "Movement"
+        verbose_name_plural = "Movements"
+    
+    def __str__(self):
+        return f"Movement N{self.id}"
+    
